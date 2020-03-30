100b1fec531b248bd3fdbd47c2bc1c0e48c36b27
Kenan Kadic <ken.kadic@outlook.com>
2020-03-30 16:38:56 -0500

    final commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,10 @@ export class AppComponent {
   title = 'orbit-report';
   
   sourceList: Satellite[]
+  displayList: Satellite[];
   constructor() {
   this.sourceList = [];
+  this.displayList = [];
   let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
 
   window.fetch(satellitesUrl).then(function(response) {
@@ -30,10 +32,21 @@ export class AppComponent {
         this.sourceList.push(satellite);
         
       }
+      this.displayList = this.sourceList.slice(0);  
      }.bind(this));
   }.bind(this));
 
 }
+search(searchTerm: string): void {
+  let matchingSatellites: Satellite[] = [];
+  searchTerm = searchTerm.toLowerCase();
+  for(let i=0; i < this.sourceList.length; i++) {
+     let name = this.sourceList[i].name.toLowerCase();
+     if (name.indexOf(searchTerm) >= 0) {
+        matchingSatellites.push(this.sourceList[i]);
+     }
+  }
+  this.displayList = matchingSatellites;
 
-
+}
 }
